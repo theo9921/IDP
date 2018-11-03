90d7efa82adbcf31917c9154b59cc7717ec6b7c0
Theodoros Ouzounellis Kavlakonis <33980928+theo9921@users.noreply.github.com>
2018-11-03 00:27:03 +0000

    Update handling.h
    Add color detection to main program

--- Software/modules/handling.h
+++ Software/modules/handling.h
@@ -40,16 +40,6 @@ void movePlatform(float position) //move platform up and down by a certain dista
 	}
 } 
 
-void collect()
-{/*
-	moveStraightLittleBitRight(2500);
-	rlink.command(WRITE_PORT_4, ACTUATORS_HALFEXT);
-	sleep(300);
-	rlink.command(WRITE_PORT_4, ACTUATORS_NOEXT);
-	*/
-}
-//void cancelCollect(){} //basically not collect
-
 
 void drop()
 {
@@ -81,8 +71,11 @@ void drop()
 	sleep(1000);
 }
 
+//return block colour
+//White=1, Red=2 Transparent=3 Wooden=4 Green=5 --> IDK = 0
 int getBlockColour()
 {
+	/*
 	int none, white, blue, red, LEN=100;
 	
 	// read none light
@@ -120,6 +113,79 @@ int getBlockColour()
 	// output normalized
 	cout << "w: " << white-none << " r: " << red-none << " b: " << blue-none << endl;
 	return 1;
+	*/
+	
+	
+	// read none light
+	rlink.command(WRITE_PORT_5, 0b11111111);
+	none = 0;
+	for(int i=0; i<LEN; i++){
+		none += rlink.request(ADC2);
+	}
+	none /= LEN;
+
+	// read white light
+	rlink.command(WRITE_PORT_5, 0b11101111);
+	white = 0;
+	for(int i=0; i<LEN; i++){
+		white += rlink.request(ADC2);
+	}
+	white /= LEN;
+
+	// read blue light
+	rlink.command(WRITE_PORT_5, 0b11011111);
+	blue = 0;
+	for(int i=0; i<LEN; i++){
+		blue += rlink.request(ADC2);
+	}
+	blue /= LEN;
+
+	// read red light
+	rlink.command(WRITE_PORT_5, 0b10111111);
+	red = 0;
+	for(int i=0; i<LEN; i++){
+		red += rlink.request(ADC2);
+	}
+	red /= LEN;
+
+	// output normalized
+	cout << "w: " << white-none << " r: " << red-none << " b: " << blue-none << endl;
+	//Green 113, 15, 85
+	if((percentageError(113, white-none)<=10) && (percentageError(15, red-none)<=40) && (percentageError(85, blue-none)<=50))
+	{
+		cout << "It is a Green Block" << endl;
+		return 5;
+	}
+	//Wood 153, 44, 136
+	if((percentageError(153, white-none)<=10) && (percentageError(44, red-none)<=40) && (percentageError(136, blue-none)<=50))
+	{
+		cout << "It is a Wooden Block" << endl;
+		return 4;
+	}
+
+	//transparent 90, 5, 33 most unstable
+	if((percentageError(90, white-none)<=10) && (percentageError(33, blue-none)<=50))
+	{
+		cout << "It is a Trasnsparent Block" << endl;
+		return 3;
+	}
+	//red 120, 28, 63
+	if((percentageError(120, white-none)<=10) && (percentageError(28, red-none)<=40) && (percentageError(63, blue-none)<=50))
+	{
+		cout << "It is a Red Block" << endl;
+		return 2;
+	}
+	//white 128, 26, 125
+	if((percentageError(128, white-none)<=10) && (percentageError(26, red-none)<=30) && (percentageError(125, blue-none)<=50))
+	{
+		cout << "It is a White Block" << endl;
+		return 1;
+	}
+	else
+	{
+		cout << "Don't know what colour" << endl;
+		return 0;
+	}
 }
 
 void scanCollect(int collectColour, int timeLen) //return the block type
